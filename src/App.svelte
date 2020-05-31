fd741edd6979215f98afeb1e1887f791f9384eec
ohalmanacs <ohalmanacs@fastmail.com>
2020-05-31 11:07:13 -0400

    WIP: Add initial input/span combo

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,40 @@
 <script>
 	export let name;
+  import { afterUpdate } from "svelte";
+
+	afterUpdate(() => {
+    let theInput = document.getElementById("the-input");
+    if (theInput) {
+      theInput.selectionStart = selStart;
+      theInput.selectionEnd = selStart;
+      theInput.focus();
+    }
+	});
+
+
+  let isEditing = false;
+  let inputValue = "this is some text that you can click on";
+  let spanValue;
+  let selStart;
+
+
+  function handleMousedown(ev) {
+    console.log(" hmd: ", ev);
+
+    /*
+    let theSpan = document.getElementById("the-span");
+    inputValue = theSpan.innerHTML;
+     */
+
+    let sel = window.getSelection();
+    console.log(" aU: ", sel);
+    console.log(" hM, anchorOffset = ", sel.anchorOffset);
+    selStart = sel.anchorOffset;
+
+    // theInput.style.display = 'inline';
+    // ev.target.style.display = 'none';
+    isEditing = true;
+  }
 </script>
 
 <style>
@@ -9,3 +44,9 @@
 </style>
 
 <h1>Hello {name}!</h1>
+
+{#if isEditing}
+  <input id="the-input" type="text" bind:value={inputValue} />
+{:else}
+  <span id="the-span" on:mousedown={handleMousedown}>{inputValue}</span>
+{/if}
